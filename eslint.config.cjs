9e78be7786069e6927057ac90fccf9ea75215bcf
acalcutt <acalcutt@techidiots.net>
2026-02-01 23:03:47 -0500

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,6 +2,7 @@
 // Merge of mapbox `base.js` + `node.js` with local overrides from .eslintrc
 const base = require('./node_modules/@mapbox/eslint-config-mapbox/base');
 const node = require('./node_modules/@mapbox/eslint-config-mapbox/node');
+const globalsList = require('globals');
 
 const localRules = {
   'space-in-parens': ['error', 'never'],
@@ -19,11 +20,30 @@ const mergedRules = Object.assign({}, base.rules || {}, node.rules || {}, localR
 module.exports = [
   {
     // base language options
-    languageOptions: {
-      parserOptions: Object.assign({}, base.parserOptions || {}, node.parserOptions || {}),
-      ecmaVersion: (base.parserOptions && base.parserOptions.ecmaVersion) || 2020,
-      env: Object.assign({}, base.env || {}, node.env || {})
-    }
+    languageOptions: (function() {
+      const parserOptions = Object.assign({}, base.parserOptions || {}, node.parserOptions || {});
+      if (!parserOptions.ecmaVersion) parserOptions.ecmaVersion = 2020;
+
+      // convert `env` into `languageOptions.globals` using the `globals` package
+      const mergedEnv = Object.assign({}, base.env || {}, node.env || {});
+      const mergedGlobals = {};
+      try {
+        Object.keys(mergedEnv).forEach((envName) => {
+          if (!mergedEnv[envName]) return;
+          const g = globalsList[envName];
+          if (g && typeof g === 'object') {
+            Object.assign(mergedGlobals, g);
+          }
+        });
+      } catch (e) {
+        // ignore if globals package not available
+      }
+
+      return {
+        parserOptions,
+        globals: mergedGlobals
+      };
+    })()
   },
   {
     // Convert plugin array into flat-config plugins object: { name: plugin }
